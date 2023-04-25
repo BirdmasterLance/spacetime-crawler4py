b07253121b00fc21876ffc2efb8870c83c1768b4
BirdmasterLance <huulego9@yahoo.com>
2023-04-25 11:09:28 -0700

    Added regex checks for valid UCI URLs
    Uses regular expressions to make sure a link matches one of the UCI ones before seeing if we can actually crawl through that page

--- scraper.py
+++ scraper.py
@@ -25,15 +25,31 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
-        return not re.match(
-            r".*\.(css|js|bmp|gif|jpe?g|ico"
-            + r"|png|tiff?|mid|mp2|mp3|mp4"
-            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
-            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
-            + r"|epub|dll|cnf|tgz|sha1"
-            + r"|thmx|mso|arff|rtf|jar|csv"
-            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+        # If the link is one of the domains we are allowed to check, 
+        # continue to the next regular expression check
+        # The actual expression is r".*((\.ics\.uci\.edu\/)|(\.cs\.uci\.edu\/)|(\.informatics\.uci\.edu\/)|(\.stat\.uci\.edu\/)).*"
+        if(re.match(
+        r".*((\.ics\.uci\.edu\/)"
+        + r"|(\.cs\.uci\.edu\/)"
+        + r"|(\.informatics\.uci\.edu\/)"
+        + r"|(\.stat\.uci\.edu\/)).*")):
+
+            # Part of the given code
+            return not re.match(
+                r".*\.(css|js|bmp|gif|jpe?g|ico"
+                + r"|png|tiff?|mid|mp2|mp3|mp4"
+                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+                + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+                + r"|epub|dll|cnf|tgz|sha1"
+                + r"|thmx|mso|arff|rtf|jar|csv"
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+        
+
+        # If the link was not valid,
+        # return False
+        return False
 
     except TypeError:
         print ("TypeError for ", parsed)
