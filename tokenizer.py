f534650a57052d5b7cfd121e6c6c55fc6651869e
BirdmasterLance <huulego9@yahoo.com>
2023-04-27 01:39:38 -0700

    Added stop words to tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,27 @@
 import builtins
 
+# From the first default stopWord list
+# found at https://www.ranks.nl/stopwords
+stopWords = [
+    'a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and',
+    'any', 'are', 'aren\'t', 'as', 'at', 'be', 'because', 'been', 'before', 'below'
+    'between', 'both', 'but', 'by', 'can\'t', 'cannot', 'couldn\'t', 'did', 'didn\'t', 'do',
+    'does', 'doesn\'t', 'doing', 'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had',
+    'hadn\'t', 'has', 'hasn\'t', 'have', 'haven\'t', 'having', 'he', 'he\'d', 'he\'ll', 'he\'s',
+    'her', 'here', 'here\'s', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'how\'s',
+    'i', 'i\'d', 'i\'ll', 'i\'m', 'i\'ve', 'if', 'in', 'into', 'is', 'isn\'t', 'it',
+    'it', 'it\'s', 'its', 'itself', 'let\'s', 'me', 'more', 'most', 'mustn\'t', 'my',
+    'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or',
+    'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 'same', 'shan\'t',
+    'she', 'she\'d', 'she\'ll', 'she\'s', 'should', 'shouldn\'t', 'so', 'some', 'such', 'than',
+    'that', 'that\'s', 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', 'there\'s',
+    'these', 'they', 'they\'d', 'they\'re', 'they\'ve', 'this', 'those', 'through', 'to', 'too',
+    'under', 'until', 'up', 'very', 'was', 'wasn\'t', 'we', 'we\'d', 'we\'ll', 'we\'re', 'we\'ve',
+    'were', 'weren\'t', 'what', 'what\'s', 'when', 'when\'s', 'where', 'where\'s', 'which', 'while',
+    'who', 'who\'s', 'whom', 'why', 'why\'s', 'with', 'won\'t', 'would', 'wouldn\'t', 'you',
+    'you\'d', 'you\'ll', 'you\'ve', 'your', 'yours', 'yourself', 'yourselves'
+    ]
+
 def tokenize(TextFilePath):
     output = list() # Initialize list
     try:
@@ -11,10 +33,10 @@ def tokenize(TextFilePath):
             if(c.isalnum() == True): # If it is alpha-numeric, add char to string
                 token = token + c
             else:
-                if(token.isalnum()): output.append(token) # Add token to output once we hit token's end
+                if(token.isalnum() and token not in stopWords): output.append(token) # Add token to output once we hit token's end
                 token = ""
             c = file.read(1)
-        if(token.isalnum()): output.append(token) # Add token to output once we hit token's end
+        if(token.isalnum() and token not in stopWords): output.append(token) # Add token to output once we hit token's end
 
         file.close() # Close file!
     except FileNotFoundError: # Catch exception
