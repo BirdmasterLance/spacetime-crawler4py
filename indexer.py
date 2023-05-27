bef9af1e56bfdf2b873bdf6a850b4fc6edd0266c
BirdmasterLance <huulego9@yahoo.com>
2023-05-27 01:20:46 -0700

    Updated Index and Indexer

--- indexer.py
+++ indexer.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from nltk.stem import PorterStemmer
 import difflib
 from urllib.parse import urldefrag
+from simhash import Simhash
 	
 import time
 
@@ -20,7 +21,7 @@ class Posting:
         self.importantScore = 0 
        
         # Save a list of what positions this word is at
-        self.position = 0
+        self.position = '0'
     
     def getWordFrequency(self):
         return self.wordFrequency
@@ -58,137 +59,163 @@ class Posting:
 # Set up the stemmer
 ps = PorterStemmer()
 
-json_dir1 = "C:\\Users\\huule\\Desktop\\School\\CS121\\DEV0"
-json_dir2 = "C:\\Users\\huule\\Desktop\\School\\CS121\\DEV1308"
-json_dir3 = "C:\\Users\\huule\\Desktop\\School\\CS121\\DEV5368"
-json_dir4 = "C:\\Users\\huule\\Desktop\\School\\CS121\\DEV7947"
-json_dir5 = "C:\\Users\\huule\\Desktop\\School\\CS121\\DEV16885"
-json_dir6 = "C:\\Users\\huule\\Desktop\\School\\CS121\\DEV20319"
+indexLimits = [0, 1307, 5367, 7946, 16884, 20318]
 
-index_file1 = "C:\\Users\\huule\\Desktop\\School\\CS121\\index0\\"
-index_file2 = "C:\\Users\\huule\\Desktop\\School\\CS121\\index1308\\"
-index_file3 = "C:\\Users\\huule\\Desktop\\School\\CS121\\index5368\\"
-index_file4 = "C:\\Users\\huule\\Desktop\\School\\CS121\\index7947\\"
-index_file5 = "C:\\Users\\huule\\Desktop\\School\\CS121\\index16885\\"
-index_file6 = "C:\\Users\\huule\\Desktop\\School\\CS121\\index20319\\"
-index_files = [index_file1, index_file2, index_file3, index_file4, index_file5, index_file6]
-
-output_file = "C:\\Users\\huule\\Desktop\\School\\CS121\\output.txt"
-
-emergency_file = "C:\\Users\\huule\\Desktop\\School\\CS121\\emergency.txt"
-
-emergency2_file = "C:\\Users\\huule\\Desktop\\School\\CS121\\emergency2.txt"
-
+json_dir = "C:\\Users\\huule\\Desktop\\School\\CS121\\DEV"
+index_file = "C:\\Users\\huule\\Desktop\\School\\CS121\\index"
 doc_index_file = "C:\\Users\\huule\\Desktop\\School\\CS121\\docIndexFile.txt"
 
 index = dict()
-
 doc_index_dict = dict()
 
-def checkHeaderForWord(soup, header, word):
-    for section in soup.find_all(header):
-        if(section.get_text() is None): continue
-        if(word in section.get_text()): return True
-    return False
+output_file = "C:\\Users\\huule\\Desktop\\School\\CS121\\output.txt"
+
+
+
+blank_space = '                                                                   '
+
+
+blacklist = [
+    '[document]',
+    'noscript',
+    'html',
+    'meta',
+    'head', 
+    'input',
+    'script',
+    'style',
+    'font',
+    'option'
+]
+
+important_tags = [
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'header',
+    'b',
+    'title',
+    'strong',
+    'em'
+]
+
+urls_visited = set()
 
 # Function to build the index
 # Set docID to change the starting ID for the documents
 def indexDocuments(docId):
+    startId = docId
     numFiles = 0
-    path = pathlib.Path(json_dir6)
+    path = pathlib.Path(json_dir + str(docId))
     totalStartTime = time.time()
 
-    for filename in path.rglob("*"):
+    for filename in path.rglob("*"): # Look through all files and directories from a single path
         numFiles += 1 # Keep track of the number of files
-        filename = str(filename)
+        filename = str(filename) # Get the string version of the file name
 
-        if filename.endswith(".json"):
+        if filename.endswith(".json"): # Check if the file is actually a JSON file
             startTime = time.time()
-            docId += 1 
 
-            # read in the JSON data from the file
+            # Read in the JSON data from the file
             with open(filename, "r") as f:
                 data = json.load(f)
 
-            # extract the text from the JSON data
+            # Extract the text from the JSON data
             content = data["content"]
-            link = data['url']
+            
+            # TODO: Implement SimHash
+
+            # Defrag the URL and compare it with future URLs to see if there are the same links down the line
+            link = data['url'] 
+            link = urldefrag(link)
+            if link in urls_visited: continue
+            else: urls_visited.add(link)
+            
             soup = BeautifulSoup(content, 'html.parser') # Parse the HTML content
 
-            # tokenize the text and build the inverted index
-            text = soup.get_text()
-            tokens = re.finditer(r'\b\w+\b', text.lower()) # Changed to find iter so we can save position of match
-            for tokenMatch in tokens:
-                token = ps.stem(tokenMatch.group()) # Stem the word to reduce repeated words
-                
-                if(len(token) == 1 or token.isnumeric()): continue
-                
-                if token not in index:
-                    index[token] = list() # Initialize a new list of postings
-                
-                # Create posting that holds document information
-                posting = Posting()
-                posting.setId(docId)
-                posting.setPosition(tokenMatch.start())
-                index[token].append(posting)
+            # Tokenize the text and build the inverted index
+            for section in soup.find_all(text=True): # Find all the text and their HTML tags in the file
+                if section.parent.name not in blacklist: # If the tag is in our blacklisted section, skip it
+                    text = section.get_text() # Get the text
 
-                # foundPosting = False
-                # if(len(index[token]) != 0):
-                #     for posting in index[token]:
-                #         if(posting.getId() == docId):
-                #             posting.setWordFrequency(posting.getWordFrequency()+1)
-                #             posting.getPositions().append(tokenMatch.start())
-                #             foundPosting = True
-                #             break
-                # if(foundPosting is False):
-                #     posting = Posting()
-                #     posting.setId(docId)
-                #     posting.setWordFrequency(1)
-                #     posting.getPositions().append(tokenMatch.start())
-                #     index[token].append(posting)
+                    tokens = re.finditer(r'\b\w+\b', text.lower()) # Changed to find iter so we can save position of match
+                    for tokenMatch in tokens: # For every token that matched our regex expression
+                        token = ps.stem(tokenMatch.group()) # Stem the word to reduce repeated words
+                        
+                        if(len(token) == 1): continue # Don't save single letters and numbers
+                        
+                        if token not in index:
+                            index[token] = list() # Initialize a new list of postings
+                        
+                        # Create posting that holds document information
+                        posting = Posting()
+                        posting.setId(docId) # Set the posting's document ID
+                        posting.setPosition(tokenMatch.start()) # Save the position of the word in the 
+                        posting.setImportantScore(section.parent.name) # Save the tag the word is in
+                        index[token].append(posting) # Append the posting to hte list
 
-                    # Check if the word is important
-                    # if(soup.find(re.compile('^h[1-6]$'), string=token) != None): posting.setImportantScore(posting.getImportantScore()+1)
-                    # elif(soup.find('b', string=token) != None): posting.setImportantScore(posting.getImportantScore()+1)
-                    # elif(soup.find('strong', string=token) != None): posting.setImportantScore(posting.getImportantScore()+1)
-                    # elif(soup.find('title', string=token) != None): posting.setImportantScore(posting.getImportantScore()+1)
-
+            # Save the document to the index of documents and document IDs
             with open(doc_index_file, 'a') as f:
                 f.write(str(docId) + ';' + filename + '\n')
+            docId += 1 
+
+            # For fun, print how long it took to complete the file
             endTime = time.time()
-            print('Execution time for', filename, ': ', str(endTime-startTime)) # For fun, print how long it took to complete the file
+            print('Execution time for', filename, ': ', str(endTime-startTime))
+    
+    # For fun, print how long it took to index the given documents
     print('ran out of files to index through')
     totalEndTime = time.time()
     print('Total execution time:', str(totalEndTime-totalStartTime))
 
-    # Put the word postings into a file
-    # Also build the index of the index
-    index_index = dict()
-    writeStartTime = time.time()
-    for token in sorted(index.keys()):
-        index_file = index_file6
-        with open(index_file + 'index' + token[0].upper() + '.txt', 'a', encoding='utf-8') as f, open(index_file + 'index' + token[0].upper() + 'Index.txt', 'a', encoding='utf-8') as f2:
-            print('saving ' + token + ' to file')
+
+
+    writeStartTime = time.time() # For fun, start time
+    indexLength = len(index) # For fun, get the current length of our index to see saving progress
+    tokenCount = 0 # For fun, get the number of tokens that need to be saved to show progress
+    
+    index_index = dict() # Create an index for our index
+    if(not pathlib.Path(index_file + str(startId)).exists()): pathlib.Path.mkdir(index_file + str(startId)) # Create folder if it does not exist
+
+    for token in sorted(index.keys()): # For every token in our index
+        # Open both the associated index (where the postings are stored) and index of index (where the position of the posting list is) file
+        with open(index_file + str(startId) + '\\index' + token[0].upper() + '.txt', 'a', encoding='utf-8') as f, open(index_file + str(startId) + '\\index' + token[0].upper() + 'Index.txt', 'a', encoding='utf-8') as f2:
+            tokenCount += 1 # For fun, get the current token we are on
+            print('saving ' + token + ' to file ({0} / {1})'.format(tokenCount, indexLength) + blank_space) # Print the saving progress
+
+            # If the letter/number is NOT in the index of index, initialize it in the dict
             if(token[0].upper() not in index_index):
                 index_index[token[0].upper()] = 0
 
-            output = token + ' '
-            numPostings = len(index[token])
+            # For fun, get the number of postings for progress
+            numPostings = len(index[token]) 
             progress = 0
+
+            # Initialize the output we will be writing to file
+            # NOTE: The output of the posting is as follow:
+            # token docID,wordPosition,tag|docID,wordPosition,tag|docID,wordPosition,tag|...
+            output = token + ' ' 
             for posting in index[token]:
-                output += '{0},{1}|'.format(str(posting.getId()), str(posting.getPosition()))
+                output += '{0},{1},{2}|'.format(str(posting.getId()), str(posting.getPosition()), str(posting.getImportantScore()))
+
+                # For fun, output progress
                 progress += 1
-                print(token + ';' + str(progress) + '/' + str(numPostings))
-                print ("\033[A                             \033[A")
+                print(token + ';' + str(progress) + '/' + str(numPostings) + blank_space, end='\r')
+            
             output = output[:-1] + '\n'
             f.write(output)
 
-            f2.write(str(index_index[token[0].upper()]) + '\n')
+            f2.write(token + ':' + str(index_index[token[0].upper()]) + '\n')
             index_index[token[0].upper()] = index_index[token[0].upper()] + len(output)
-            print ("\033[A                                                                                                   \033[A")
+            print('', end='\033[F')
+    print()
+
+    # For fun, output how long it took to save the index to a file
     writeEndTime = time.time()
     print('Time it took to write to file:', str(writeEndTime-writeStartTime))
-    print('Writing doc ids to docs')
     print('====== END ======')
 
 # Returns the posting information from a word based on its info in the index file
@@ -197,35 +224,40 @@ def getWordPostingFromFile(startingWord):
     startingWord = startingWord.lower() # Set the word to be all lower case
     startingLetter = startingWord[0].upper() # Get the first letter of the starting word
     postingList = list()
-    for index_file in index_files: # For the all index files we have
+    for indexNum in indexLimits: # For the all index files we have
+        index_file2 = index_file + str(indexNum)
         try:
             # Open both the index file and the index of the index file
-            with open(index_file + 'index' + startingLetter + '.txt', 'r', encoding='utf-8') as f, open(index_file + 'index' + startingLetter + 'Index.txt', 'r', encoding='utf-8') as f2:
+            with open(index_file2 + '\\index' + startingLetter + '.txt', 'r', encoding='utf-8') as f, open(index_file2 + '\\index' + startingLetter + 'Index.txt', 'r', encoding='utf-8') as f2:
                 if(f is None or f2 is None): continue # If the file doesn't exist, continue
 
                 index_line = f2.readline() # Read the line of the index of the index file
                 limit = 0
                 while index_line: # While there is a line to read
-                    indexPosition = int(index_line) # Get the position of where the word starts in the index file
-                    if(indexPosition != 0): 
-                        indexPosition += limit
-                    
-                    f.seek(indexPosition) # Get the position of the word in the index file
-                    postingLine = f.readline() # Read where the word line is
-                    postingInfo = postingLine.split(' ') # Split it so we have the word and its posting
-
-                    # Because special letters have a length greater than 1
-                    # We need to adjust the position so the seek is correct
+                    indexSplit = index_line.split(':')
+                    indexWord = indexSplit[0]
+                    indexPosition = int(indexSplit[1]) # Get the position of where the word starts in the index file
+                    # Temporary fix because index of index is wrong
+                    indexPosition += limit
                     limit += 1
-                    notABC = (char for char in postingInfo[0] if char not in 'abcdefghijklmnopqrstuvwxyz0123456789|,\n-_\'')
-                    for char in notABC: limit += 1
-
+                    # print(indexPosition)
+                    
                     # Use this library to get the nearest similarity to the word
-                    if(difflib.get_close_matches(startingWord, [postingInfo[0]], cutoff=0.85)):
+                    if(difflib.get_close_matches(startingWord, [indexWord], cutoff=0.85)):
+                        f.seek(indexPosition) # Get the position of the word in the index file
+                        postingLine = f.readline() # Read where the word line is
+                        
+                        postingInfo = postingLine.split(' ') # Split it so we have the word and its posting
+                        
+                        # Because special letters have a length greater than 1
+                        # We need to adjust the position so the seek is correct
+                        notABC = (char for char in postingInfo[0] if char not in 'abcdefghijklmnopqrstuvwxyz0123456789|,\n-_\'')
+                        for char in notABC: limit += 1
+
                         wordInfo = postingInfo[1].split('|') # The postings are split with '|' 
                         for word in wordInfo:
                             idAndPosition = word.split(',') # The document ID and word position is split with ','
-                            
+                            if(len(idAndPosition) != 3): continue # Sometimes we get values that are not what we want, so skip them
                             # Create and populate the postings
                             posting = Posting() 
                             posting.setId(int(idAndPosition[0]))
@@ -295,9 +327,12 @@ def printURLs(collection, limiter):
     return len(urls)
 
 if __name__ == '__main__':
+    # indexDocuments(indexLimits[5])
     query = input("Please enter a query: ")
+    startTime = time.time()
     test = list()
     for word in query.split(' '):
+        word = ps.stem(word)
         postingDict = getWordPostingFromFile(word)
         postingFreq = getDocFrequencyFromPosting(postingDict)
         test2 = list()
@@ -317,6 +352,8 @@ if __name__ == '__main__':
             count += 1
         limit = printURLs(compare, 5)
         if limit < 5: printURLs(test[0], 5-limit)
+    endTime = time.time()
+    print("Query Time:", str((endTime-startTime)*1000), 'ms')
 
 
 # Write to the output file
